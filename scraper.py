f15f2053a385229810585b2535ebf7e6b9ccd8a7
marketintelligencejgi <marketintelligence.jgi@gmail.com>
2026-03-19 11:47:05 -0500

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -24,6 +24,8 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-blink-features=AutomationControlled")
 
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option("useAutomationExtension", False)
 
@@ -250,4 +252,3 @@ msg.add_attachment(
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
     smtp.login(sender, password)
     smtp.send_message(msg)
-    
