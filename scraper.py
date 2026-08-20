ddeaa28d1aa63ea5f3271e76bc4f3434719ca5b0
marketintelligencejgi <marketintelligence.jgi@gmail.com>
2026-08-20 09:09:40 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -47,50 +47,19 @@ time.sleep(random.uniform(1.5, 3.5))
 driver.get('https://www.metal.com/')
 time.sleep(random.uniform(8, 14))
 
-# 1. Cerrar aviso de cookies
-cookie_dialogs = driver.find_elements(
-    By.CSS_SELECTOR,
-    "[role='dialog']"
-)
-
-if cookie_dialogs:
-
-    reject_button = cookie_dialogs[0].find_element(
-        By.CSS_SELECTOR,
-        "button[data-role='necessary']"
-    )
-
-    reject_button.click()
-
-    time.sleep(2)
-
-# 2. Buscar login
-input_user = WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located(
-        (By.CSS_SELECTOR, "input[autocomplete='username']")
-    )
-)
-
-input_password = WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located(
-        (By.CSS_SELECTOR, "input[name='password']")
-    )
-)
-
-button_login = WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located(
-        (By.CSS_SELECTOR, "button.smm-auth-submit")
-    )
-)
-
 # Sign in
 boton = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(@class,'signInButton')]")))
 boton.click()
 time.sleep(random.uniform(3.5, 6.5))
 
+input_user = driver.find_element(By.XPATH, '//*[@id="_r_0_"]')
+input_pass = driver.find_element(By.XPATH, '//*[@id="_r_2_"]')
+boton = driver.find_element(By.XPATH, '//*[@id="action"]/div[1]/div[1]/div/div[1]/form/div[4]/div/div/div/div/button')
+# boton = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//div[contains(@class,'loginWrapper') and contains(@class,'modal')]//form/div[4]//button")))
+
 input_user.send_keys(user)
 input_pass.send_keys(password)
-button_login.click()
+boton.click()
 
 del(user, password, input_user, input_pass, boton)
 
